527ed3634c32b69cc222fc3f785c4bc99d56a903
Sayan Sivakumaran <sivakusayan@gmail.com>
2018-09-30 05:45:42 -0400

    Added ESLint. Setup JSdoc and basic outline for fetching API data

--- src/data/eventData.js
+++ src/data/eventData.js
@@ -1,12 +1,43 @@
-const axios = require('axios');
+/**
+ * @fileoverview Fetches the list of recent events for the application to use.
+ * 
+ * @module src/data/eventData
+ */
+import axios from 'axios';
 
-const eonetURL = 'https://eonet.sci.gsfc.nasa.gov/api/v2.1/events';
+/**
+ * Fetches the data from the EONET API.
+ * @function
+ * 
+ * @returns {string} A JSON list of EONET events.
+ */
+const fetchEonetData = () => {
+  const eonetURL = 'https://eonet.sci.gsfc.nasa.gov/api/v2.1/events';
+  axios.get(eonetURL)
+    .then(response => console.log(response.data.events))
+    .catch((err) => {
+      console.log('Something went wrong :(');
+      console.log(err);
+    });
+};
 
-axios.get(eonetURL)
-.then((response) => {
-  console.log(response.data.events.length);
-})
-.catch((err) => {
-  console.log('Something went wrong :(');
-  console.log(err);
-})
+/**
+ * Converts the JSON from the EONET API into usable GeoJSON.
+ * @function
+ * 
+ * @param {String} eonetJson The JSON received from the EONET API.
+ * 
+ * @returns {String} A GeoJSON representation of EONET data.
+ */
+const convertData = (eonetJson) => {
+  console.log(eonetJson);
+};
+
+/**
+ * Fetches events from the EONET API and converts them into GeoJSON.
+ * @name fetchEvents
+ * @function
+ * 
+ * @returns {string} A GeoJSON list of natural events.
+ */
+export default () => convertData(fetchEonetData());
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: './src/data/eventData.js',
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'

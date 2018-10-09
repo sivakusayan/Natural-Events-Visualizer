2de39326c8e25ad1ea462b1d1ebfd4101822990e
Sayan Sivakumaran <sivakusayan@gmail.com>
2018-10-09 09:37:29 -0400

    Basic functionality of reverseGeocoding

--- server/processors/reverseGeocode.js
+++ server/processors/reverseGeocode.js
@@ -5,17 +5,32 @@ const geocoder = require('local-reverse-geocoder');
 const path = require('path');
 
 const fetchEvents = require('./fetchEvents');
+const getCountryName = require('./countryName');
 
-const reverseGeocode = (geometry) => {
+const reverseGeocode = ([longitude, latitude]) => {
   // Check if coordinate is in ocean
-  // const ocean = checkExtremes(geometry.coordinates);
+  // const ocean = checkOcean(geometry.coordinates);
   // if (ocean) console.log({ main: ocean });
 
   // If not in ocean, do typical lookup
-  geocoder.lookUp(geometry.coordinates, (err, res) => {
+  geocoder.lookUp({ latitude, longitude }, (err, res) => {
     const data = res[0][0];
-    console.log({ city: data.name, province: data.admin1code, country: data.countryCode });
+    console.log([latitude, longitude]);
+    console.log({
+      city: data.name,
+      province: data.admin1Code !== null ? data.admin1Code.name : null,
+      country: getCountryName(data.countryCode),
+    });
   });
+};
+
+const pointMean = (points) => {
+  let [xSum, ySum] = [0, 0];
+  points.forEach((point) => {
+    xSum += point[0];
+    ySum += point[1];
+  });
+  return [xSum / points.length, ySum / points.length];
 };
 
 const attachReverseGeocode = async (geoJSON) => {
@@ -27,7 +42,14 @@ const attachReverseGeocode = async (geoJSON) => {
     },
     dumpDirectory: path.join(__dirname, '../geonames'),
   }, () => {
-    geoJSON.forEach(event => reverseGeocode(event.geometries[0].coordinates));
+    geoJSON.forEach((event) => {
+      const coordinates = event.geometries[0].coordinates;
+      if (coordinates[0].constructor === Array) {
+        reverseGeocode(pointMean(coordinates[0]));
+      } else {
+        reverseGeocode(coordinates);
+      }
+    });
   });
 };
 
@@ -35,4 +57,4 @@ const attachReverseGeocode = async (geoJSON) => {
   attachReverseGeocode(await fetchEvents());
 })();
 
-module.exports.attachReverseGeocode = attachReverseGeocode;
+// module.exports.attachReverseGeocode = attachReverseGeocode;

907f2fb338a4117cab93c1667727f98297fa7da7
Sayan Sivakumaran <sivakusayan@gmail.com>
2018-11-04 04:48:37 -0500

    Removed api keys from history

--- constants/PRECISION.js
+++ constants/PRECISION.js
@@ -1,9 +1,12 @@
 /**
- * We will round the coordinates of our events to 4 decimal places. According to the 
- * Wikipedia page at: https://en.wikipedia.org/wiki/Decimal_degrees and the discussion 
- * at https://gis.stackexchange.com/questions/8650/measuring-accuracy-of-latitude-and-longitude,
- * this should be sufficient to distinguish two different parcels of land, which is more 
- * than enough for our needs.
+ * @fileoverview Holds the value that determines number of decimal places that we
+ * round coordinates to. We round the event coordinates to exchange unnecessary
+ * precision for performance.
+ * 
+ * According to the discussion at https://gis.stackexchange.com/questions/8650/measuring-accuracy-of-latitude-and-longitude
+ * and https://en.wikipedia.org/wiki/Decimal_degrees, coordinates that are rounded to 
+ * 4 decimal places are accurate enough to distinguish two parcels of land. This is
+ * more than enough for our needs.
  */
 
 module.exports = 4;
--- constants/URL_STRINGS.js
+++ constants/URL_STRINGS.js
@@ -1,8 +1,6 @@
-// URL to categories endpoint of EONET API
-module.exports.EONET_CATEGORIES_URL = 'https://eonet.sci.gsfc.nasa.gov/api/v2.1/categories';
-// URL to events endpoint of EONET API
+/**
+ * @fileoverview Holds the URL strings that the application sends requests to.
+ */
+
 module.exports.EONET_EVENTS_URL = 'https://eonet.sci.gsfc.nasa.gov/api/v2.1/events';
-// URL to reverse geocoding API
 module.exports.GOOGLE_REVERSE_GEOCODE_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
-// URL to events endpoint of our database
-module.exports.EVENTS_URL = 'http://localhost:3000/api/events';

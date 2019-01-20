4c378bda27745e66d0fb83602a7eeaa0a3f9a1bb
Sayan Sivakumaran <sivakusayan@gmail.com>
2019-01-20 06:35:36 -0500

    Styled headers and blockquotes. Added room on bottom of every page for button

--- client/src/components/Pages/Credits.js
+++ client/src/components/Pages/Credits.js
@@ -10,7 +10,7 @@ const Credits = () => (
   <section
     className='article'
   >
-    <Link className='bigButton bigButton--right' to='/menu' title='Back to menu' alt='Back to menu'>
+    <Link className='bigButton bigButton--left' to='/menu' title='Back to menu' alt='Back to menu'>
       <svg className='bigButton__icon'>
         <use href='icons/sprite.svg#backArrow' />
       </svg>
